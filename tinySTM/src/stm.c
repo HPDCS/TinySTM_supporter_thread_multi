a974dd03c038a20b6f93e5e08012d699540442d9
io <disanzo@dis.uniroma1.it>
2017-03-09 16:00:20 +0100

    a

--- tinySTM/src/stm.c
+++ tinySTM/src/stm.c
@@ -1637,7 +1637,7 @@ void run_supporter_thread(void * data) {
 			printf("\nnow: %llu\tstm_tx_pointer->end: %llu", now, stm_tx_pointer->end);
 		*/
 
-			stm_tx_pointer->current_run_checked=1;
+			//stm_tx_pointer->current_run_checked=1;
 			/*
 			//pthread_spin_lock(&test_spinlock);
 			//int g=_stm_validate(main_stm_tx);
